70629f836d3f12326b95080619bccdc59a498692
rumiaktar <ce.rumiakter@gmail.com>
2024-03-28 00:05:55 +0600

    add footer, header, job data

---
+++ src/components/Job/Job.jsx
@@ -0,0 +1,23 @@
+
+
+const Job = ({ job }) => {
+    const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary, job_description } = job;
+    return (
+        <div className="card card-compact bg-base-100 shadow-xl">
+            <figure><img src={logo} alt="Shoes" /></figure>
+            <div className="card-body">
+                <h2 className="card-title">{job_title}</h2>
+                <p>{company_name}</p>
+                <div>
+                    <button className="px-5 py-2 font-bold border rounded border-[#7E90FE] mr-4 text-[#6c7ffb]">{remote_or_onsite}</button>
+                    <button className="px-5 py-2 font-bold border rounded border-[#7E90FE] text-[#6c7ffb]">{job_type}</button>
+                </div>
+                <div className="card-actions">
+                    <button className="btn btn-primary">View Details</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Job;
