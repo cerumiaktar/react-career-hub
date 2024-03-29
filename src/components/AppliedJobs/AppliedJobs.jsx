b0c9cf7b8e29deb8c5874266885738f74b7c738c
rumiaktar <ce.rumiakter@gmail.com>
2024-03-30 00:59:39 +0600

    add dropdown

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -14,7 +14,12 @@ const AppliedJobs = () => {
             setDisplayJobs(appliedJobs);
         }
         else if (filter === 'remote'){
-            const remoteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Remote')
+            const remoteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Remote');
+            setDisplayJobs(remoteJobs);
+        }
+        else if(filter === 'onsite'){
+            const onsiteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Onsite');
+            setDisplayJobs(onsiteJobs);
         }
     }
 
@@ -41,9 +46,9 @@ const AppliedJobs = () => {
             <details className="dropdown">
                 <summary className="m-1 btn">open or close</summary>
                 <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-                    <li><a>All</a></li>
-                    <li><a>Remote</a></li>
-                    <li><a>On-Site</a></li>
+                    <li onClick={() => handleJobsFilter('all')}><a>All</a></li>
+                    <li onClick={() => handleJobsFilter('remote')}><a>Remote</a></li>
+                    <li onClick={() => handleJobsFilter('onsite')}><a>On-Site</a></li>
                 </ul>
             </details>
             <ul>
